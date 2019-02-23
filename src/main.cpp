1e5229d6f5e405259c4666c9bcbc739a0c82fc1b
Jeff Wright <jeffw387@gmail.com>
2019-02-23 11:16:34 -0800

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -134,7 +134,8 @@ int main() {
     vka::render_pass_builder{}
       .add_attachment(
         vka::attachment_builder{}
-          .initial_layout(VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
+          .initial_layout(
+            VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
           .final_layout(VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
           .format(swapFormat)
           .loadOp(VK_ATTACHMENT_LOAD_OP_CLEAR)
@@ -143,17 +144,20 @@ int main() {
           .build())
       .add_subpass(
         vka::subpass_builder{}
-          .color_attachment(0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
+          .color_attachment(
+            0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
           .build())
       .build(*devicePtr)
       .map_error(err::crit{"Error creating render pass!"})
       .value();
-  auto blendState = vka::make_blend_state(
-    {vka::make_blend_attachment(vka::no_blend_attachment{})});
-  auto depthStencilState = vka::make_depth_stencil_state(false, false);
+  auto blendState =
+    vka::make_blend_state({vka::make_blend_attachment(
+      vka::no_blend_attachment{})});
+  auto depthStencilState =
+    vka::make_depth_stencil_state(false, false);
   auto dynamicState = vka::make_dynamic_state();
-  auto inputAssemblyState =
-    vka::make_input_assembly(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+  auto inputAssemblyState = vka::make_input_assembly(
+    VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
   VkViewport viewport{};
   viewport.width = static_cast<float>(surfaceWidth);
   viewport.height = static_cast<float>(surfaceHeight);
@@ -161,14 +165,20 @@ int main() {
   viewport.maxDepth = 1.f;
 
   VkRect2D scissor{};
-  scissor.extent.width = static_cast<uint32_t>(surfaceWidth);
-  scissor.extent.height = static_cast<uint32_t>(surfaceHeight);
-  auto viewportState = vka::make_viewport_state({viewport}, {scissor});
+  scissor.extent.width =
+    static_cast<uint32_t>(surfaceWidth);
+  scissor.extent.height =
+    static_cast<uint32_t>(surfaceHeight);
+  auto viewportState =
+    vka::make_viewport_state({viewport}, {scissor});
   auto rasterizationState = vka::make_rasterization_state();
-  auto vertexStageState = vka::make_shader_stage(vertexShaderData, "main", {});
+  auto multisampleState = vka::make_multisample_state();
+  auto vertexState =
+    vka::make_vertex_state(vertexShaderData.shaderData);
+  auto vertexStageState =
+    vka::make_shader_stage(vertexShaderData, "main", {});
   auto fragmentStageState =
     vka::make_shader_stage(fragmentShaderData, "main", {});
-  auto vertexState = vka::make_vertex_state(vertexShaderData.shaderData);
   auto pipelinePtr = vka::make_pipeline(
     *devicePtr,
     *renderPassPtr,
